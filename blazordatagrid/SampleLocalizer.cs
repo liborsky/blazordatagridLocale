34f8f2bd098cf207d86f883cbbc95a5e2262d7f0
Libor Hynek <libor.hynek@gmail.com>
2020-04-30 10:38:31 +0200

    Add project files.

---
+++ blazordatagrid/SampleLocalizer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Syncfusion.Blazor;
+
+namespace blazordatagrid
+{
+    public class SampleLocalizer : ISyncfusionStringLocalizer
+    {
+
+        public string Get(string key)
+        {
+            return this.Manager.GetString(key);
+        }
+
+        public System.Resources.ResourceManager Manager
+        {
+            get
+            {
+                return  blazordatagrid.Resources.SyncfusionBlazorLocale.ResourceManager;
+            }
+        }
+    }
+}
---
+++ blazordatagrid/Startup.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Hosting;
+using blazordatagrid.Data;
+using Syncfusion.Blazor;
+using System.Globalization;
+using Microsoft.Extensions.Options;
+
+namespace blazordatagrid
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSyncfusionBlazor();
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                //    // define the list of cultures your app will support
+                var supportedCultures = new List<CultureInfo>()
+            {
+                new CultureInfo("en-US"),
+                new CultureInfo("de-DE")
+            };
+
+                // set the default culture
+                options.DefaultRequestCulture = new RequestCulture("de-DE");
+
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+                options.RequestCultureProviders = new List<IRequestCultureProvider>() { 
+                 new QueryStringRequestCultureProvider()
+                };
+            });
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton(typeof(ISyncfusionStringLocalizer), typeof(SampleLocalizer));
+            services.AddSingleton<WeatherForecastService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //#region Localization
+
+            app.UseRequestLocalization();
+
+            //#endregion
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
